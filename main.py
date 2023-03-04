331e4871ee2da1a3e537a151dd089730f8f2f766
Paul Rijneveld <paulrijneveld@gmail.com>
2023-03-04 14:39:58 +0000

    Calculate board over time

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from queue import PriorityQueue
 # https://docs.python.org/3/library/queue.html
 
@@ -19,6 +20,18 @@ class Pos:
     def dist(self, target) -> int:
         return abs(self.x - target.x) + abs(self.y - target.y)
 
+    def move_left(self, steps) -> Pos:
+        return Pos(self.x - steps, self.y)
+
+    def move_right(self, steps) -> Pos:
+        return Pos(self.x + steps, self.y)
+
+    def move_up(self, steps) -> Pos:
+        return Pos(self.x, self.y - steps)
+
+    def move_down(self, steps) -> Pos:
+        return Pos(self.x, self.y + steps)
+
 
 class Valley:
 
@@ -29,19 +42,45 @@ class Valley:
         self.target = Pos(self.width - 2, self.height - 1)
         self.lines = lines
 
-    def get_pos(self, pos) -> str:
+    def get_pos_initial(self, pos) -> str:
         return self.lines[pos.y][pos.x]
 
-    def get_pos(self, pos, minute) -> str:
-        left_on_pos
+    def get_pos(self, pos, minutes) -> str:
+        left_pos = self.wrap_to_board_pos(pos.move_left(minutes))
+        right_pos = self.wrap_to_board_pos(pos.move_right(minutes))
+        up_pos = self.wrap_to_board_pos(pos.move_up(minutes))
+        down_pos = self.wrap_to_board_pos(pos.move_down(minutes))
+        left = self.get_pos_initial(left_pos)
+        right = self.get_pos_initial(right_pos)
+        up = self.get_pos_initial(up_pos)
+        down = self.get_pos_initial(down_pos)
+        on_pos = []
+        if (left == '>'):
+            on_pos.append(left)
+        if (right == '<'):
+            on_pos.append(right)
+        if (up == 'v'):
+            on_pos.append(up)
+        if (down == '^'):
+            on_pos.append(down)
+        on_pos_count = len(on_pos)
+        if (on_pos_count == 0):
+            return '.'
+        if (on_pos_count == 1):
+            return on_pos[0]
+
+        return str(on_pos_count)
 
     def is_open(self, pos) -> bool:
         return self.lines[pos.y][pos.x] == '.'
 
-    def print_valley(self):
+    def print_valley(self, minutes):
         for y in range(self.height):
             for x in range(self.width):
-                print(self.get_pos(Pos(x, y)), end='')
+                if (x == 0 or x == self.width - 1 or y == 0 or y == self.height - 1):
+                    print(self.get_pos_initial(Pos(x, y)), end='')
+                else:
+                    print(self.get_pos(Pos(x, y), minutes), end='')
             print()
 
     def wrap_to_board_pos(self, pos) -> Pos:
@@ -52,16 +91,9 @@ class Valley:
 with open('Example.txt') as file:
     valley = Valley(file.read().splitlines())
 
-valley.print_valley()
+for n in range(10):
+    print(f"=== Minute {n} ===")
+    print()
+    valley.print_valley(n)
+    print()
 
-
-def test_is_open(x, y):
-    pos = Pos(x, y)
-    open = valley.is_open(pos)
-    print(f"Pos {pos} is open={open}")
-
-
-test_is_open(3, 1)
-test_is_open(2, 1)
-test_is_open(4, 1)
-test_is_open(1, 2)
